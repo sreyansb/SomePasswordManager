9682645edaf24e41b1c30e280820699cb4d14191
Sreyans <sreyansrocks@gmail.com>
2022-08-15 16:51:20 +0530

    First version, many more to follow, hopefully!

---
+++ config_file.py
@@ -0,0 +1,3 @@
+password_file = "passwords.tsv"
+key_file = "very_important_key.key"
+zeroth_line = "\t0th Line UseCase\t0th Line UserId\t0th Line Password"
---
+++ password_manager.py
@@ -0,0 +1,113 @@
+from msilib.schema import Error
+import os.path
+from cryptography.fernet import Fernet
+from cryptography.fernet import InvalidToken
+import argparse
+import config_file
+import json
+
+class Singleton_Fernet:
+    __instance = None
+    def __init__(self):
+        filekey = ""
+        with open(config_file.key_file,"rb") as file:
+            filekey = file.read()
+        Singleton_Fernet.__instance = Fernet(filekey)
+    def instance(self):
+        if not(Singleton_Fernet.__instance):
+            Singleton_Fernet()
+        return Singleton_Fernet.__instance
+
+def generate_parser():
+    parser = argparse.ArgumentParser(description= "-r for read, -w for write, -i for initialize")
+    parser.add_argument("-r","--read",help = "read password")
+    parser.add_argument("-w","--write",help = "write passwords")
+    parser.add_argument("-i","--initialize",help = "initialize manager")
+    return parser
+
+def is_zeroth_line_same(zeroth_line,singleton_fernet: Singleton_Fernet):
+    with open(config_file.password_file,"rb") as file:
+        zero_line = file.readline()
+    decrypted_zero_line = singleton_fernet.instance().decrypt(zero_line)
+    return decrypted_zero_line == zeroth_line
+
+def read():
+    required_use_case = input("Enter the use-case : ").lower()
+    s = Singleton_Fernet()
+    if required_use_case != "all":
+        with open(config_file.password_file,"rb") as file:
+            try:
+                while(file):
+                    use_case = s.instance().decrypt(file.readline()).decode().split("\t")
+                    if use_case[0] == required_use_case:
+                        use_case = {"USE_CASE":use_case[0],"USERNAME":use_case[1],"PASSWORD":use_case[2]}
+                        print(use_case)
+                        break
+            except InvalidToken as E:
+                return
+    else:
+        use_cases = []
+        with open(config_file.password_file,"rb") as file:
+                while(file):
+                    try:
+                        use_cases.append(s.instance().decrypt(file.readline()).decode().split("\t"))
+                    except InvalidToken as E:
+                        break
+        result = []
+        heading = ["USE_CASE","USERNAME","PASSWORD"]
+        for use_case in use_cases[1:]:
+            result.append({heading[index]:use_case[index] for index in range(3)})
+        print(json.dumps(result))
+
+def write():    
+    use_case = input("Enter the use-case : ")
+    user_id = input("Enter the user-id : ")
+    use_case_password = input("Enter the password of the application : ")
+    line = f"{use_case.lower()}\t{user_id}\t{use_case_password}"
+    s = Singleton_Fernet()
+    with open(config_file.password_file,"ab") as file:
+        file.write(s.instance().encrypt(line.encode()))
+        file.write("\n".encode())
+
+
+def initialize(password):
+    if os.path.isfile(config_file.password_file):
+        regen = input("A password file already exists. Press Y if you want to rewrite : ")
+        if regen.upper != "Y":
+            return
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    with open(config_file.key_file,"wb") as file:
+        file.write(key)
+    first_line = f"{password}{config_file.zeroth_line}"
+    with open(config_file.password_file,"wb") as file:
+        file.write(fernet.encrypt(first_line.encode()))
+        file.write("\n".encode())
+
+def main():
+    parser = generate_parser()
+    args = parser.parse_args()
+    if not(args.read or args.write or args.initialize):
+        return
+    password = args.read or args.write or args.initialize
+    if args.initialize:
+        initialize(password)
+        return
+    if not(os.path.isfile(config_file.password_file)):
+        print("Manager not initialized")
+        return
+    s = Singleton_Fernet()
+    zeroth_line = password+config_file.zeroth_line
+    if not(is_zeroth_line_same(zeroth_line.encode(),s)):
+        print("Given Password doesn't match manager's password")
+        return
+    if args.read:
+        read()
+        return
+    if args.write:
+        write()
+        return
+    pass
+
+if __name__ == "__main__":
+    main()
